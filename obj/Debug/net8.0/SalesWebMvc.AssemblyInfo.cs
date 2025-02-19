3eaa3a90358e1d4f2777f0d0b1639071990cd8a6
Thiago Ferreira do Prado <95104754+ThiagoFerreiradoPrado@users.noreply.github.com>
2025-02-19 15:33:47 -0300

    removendo arquivos para fazer alterações !

--- obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc58f540f3bbe2254dbd99ab1c26b254a872c21b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
