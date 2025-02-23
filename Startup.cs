4e7f6653e728dc2387f1059444d43cab9055795b
Thiago Ferreira do Prado <95104754+ThiagoFerreiradoPrado@users.noreply.github.com>
2025-02-23 15:09:35 -0300

    seeding service

--- Startup.cs
+++ Startup.cs
@@ -2,19 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Globalization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
+using Microsoft.Extensions.Options;
 using SalesWebMvc.Data;
-
 
 namespace SalesWebMvc {
     public class Startup {
@@ -25,10 +23,8 @@ namespace SalesWebMvc {
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) 
-            {
-            services.Configure<CookiePolicyOptions>(options => 
-            {
+        public void ConfigureServices(IServiceCollection services) {
+            services.Configure<CookiePolicyOptions>(options => {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
@@ -38,22 +34,18 @@ namespace SalesWebMvc {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesWebMvcContext>(options =>
-                 options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
-                    builder.MigrationsAssembly("SalesWebMvc")));
-
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
+                            builder.MigrationsAssembly("SalesWebMvc")));
+            services.AddScoped<SeedingServirce>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) 
-            {
-           
-            if (env.IsDevelopment()) 
-                {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingServirce seedingServirce) {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
-                
+                seedingServirce.Seed();
             }
-            else 
-            {
+            else {
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
@@ -62,8 +54,7 @@ namespace SalesWebMvc {
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseMvc(routes => 
-            {
+            app.UseMvc(routes => {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
